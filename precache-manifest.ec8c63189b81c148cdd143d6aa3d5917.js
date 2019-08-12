3dfc8b9d12b52ccb7c4f2ad032e0c190255112c2
Gloria Cho <gcho91@gmail.com>
2019-08-12 01:00:26 -0500

    Updates

---
+++ precache-manifest.ec8c63189b81c148cdd143d6aa3d5917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becd57f237dd97e52e2ba477acd031b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe8697cfd84352f1367",
+    "url": "/static/css/main.bd925bdb.chunk.css"
+  },
+  {
+    "revision": "36e77f2ed6b8c2922bbd",
+    "url": "/static/js/2.f65dfcc4.chunk.js"
+  },
+  {
+    "revision": "8fe8697cfd84352f1367",
+    "url": "/static/js/main.5e18206c.chunk.js"
+  },
+  {
+    "revision": "a54b0f95444ec3116562",
+    "url": "/static/js/runtime~main.b640acbc.js"
+  }
+]);
